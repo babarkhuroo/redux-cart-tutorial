b09fd997e419520aa2b92365c5fe56d52a7632d4
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2022-01-01 18:02:08 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+// components
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
+// items
+import cartItems from './cart-items'
+// redux stuff
+import { createStore } from 'redux'
+import { INCREASE, DECREASE } from './actions'
+import reducer from './reducer'
+import { Provider } from 'react-redux'
+
+// initial store
+const initialStore = {
+  cart: cartItems,
+  total: 105,
+  amount: 5,
+}
+
+// create store
+const store = createStore(reducer, initialStore)
+
+function App() {
+  // cart setup
+
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer />
+    </Provider>
+  )
+}
+
+export default App
